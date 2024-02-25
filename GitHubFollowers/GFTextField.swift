cceb4fdb777d92af057020499b27e7d96af7c81b
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-25 15:34:23 +0300

    feat: create text field

---
+++ GitHubFollowers/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Lee Sangoroh on 25/02/2024.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        ///black - light mode & white - dark mode
+        textColor = .label
+        ///cursor color
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
