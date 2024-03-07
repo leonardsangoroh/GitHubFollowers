b612563bacd759702854c9bf4bb994bd1cdac294
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-03-07 19:03:20 +0300

    refactor: place error messages in an enum

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -21,7 +21,7 @@ class FollowerListVC: UIViewController {
         
         NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
             guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!, buttonTitle: "OK")
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!.rawValue, buttonTitle: "OK")
                 return
             }
             
