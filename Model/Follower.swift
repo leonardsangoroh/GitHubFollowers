a4a1981c0c72079a8efd859191de48ec1b8f85e2
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-03-07 18:04:35 +0300

    bug: models and managers are not showing

---
+++ Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Lee Sangoroh on 04/03/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
