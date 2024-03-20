82e6b17fcb6329821a1f8b81cc8422389cc6b09d
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-03-20 22:49:05 +0300

    feat: network calls using Result type

---
+++ GitHubFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCell.swift
+//  GitHubFollowers
+//
+//  Created by Lee Sangoroh on 07/03/2024.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+}
---
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Lee Sangoroh on 07/03/2024.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
