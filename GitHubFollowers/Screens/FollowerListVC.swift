432849672c424a6480baa9a2436118b424566971
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-03-25 10:10:44 +0300

    feat: automatic reference counting, manage memory leaks, minimal refactoring

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -50,33 +50,25 @@ class FollowerListVC: UIViewController {
     
     func configureCollectionView(){
         // view.bounds fills up the whole view
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
-    func createThreeColumnFlowLayout() -> UICollectionViewLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding*2) - (minimumItemSpacing*2)
-        let itemWidth = availableWidth/3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
     
     
     func getFollowers(){
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        // introduce [weak self] capture list
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
             
-            
+            // unwrapping self optional
+            // - now we are safe from memory leaks
+            guard let self = self else {return}
             switch result {
                 case .success(let followers):
+                    // network call has a strong reference to 'self' (FollowerListVC) which could cause a memory leak
+                    // solution is to make self weak, which in turn makes it an optional
                     self.followers = followers
                     print(followers)
                     self.updateData()
